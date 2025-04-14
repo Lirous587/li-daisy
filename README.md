5756eeffcb7f60180b286a6522b5044ec0cd4088
Lirous587 <2115883273@qq.com>
2025-04-14 10:46:47 +0800

    配置添加

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# li-daisy-dev
+# li-daisy
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- examples/main.ts
+++ examples/main.ts
@@ -4,11 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// 直接从源码导入，不依赖构建文件
-import { LiDaisy } from 'li-daisy'
-import 'li-daisy/dist/style.css'
-// 如果有style.css文件
-// import '../dist/style.css'
+import './assets/css/tailwind.css'
+
+import { LiDaisy } from '../packages/index'
+import '../dist/style.css'
 
 const app = createApp(App)
 
