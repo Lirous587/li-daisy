fa36aa0e9c67614d293f11c489b641e54337269c
Lirous587 <2115883273@qq.com>
2025-04-20 11:41:23 +0800

    vitepress接入ThemController

--- docs/.vitepress/theme/store/theme.ts
+++ docs/.vitepress/theme/store/theme.ts
@@ -1,54 +1,24 @@
 import { defineStore } from 'pinia'
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
 export const useMyThemeStore = defineStore('myThemeStore', () => {
   const theme = ref('default')
 
-  // 所有暗色主题
-  const darkThemes = [
-    'dark',
-    'synthwave',
-    'halloween',
-    'forest',
-    'aqua',
-    'black',
-    'luxury',
-    'dracula',
-    'business',
-    'night',
-    'coffee',
-    'dim',
-    'sunset',
-    'abyss',
-  ]
-
-  const initMode = () => {
-    const nowTheme = localStorage.theme || 'default'
-    setMode(nowTheme)
+  const initTheme = () => {
+    const nowTheme = localStorage.theme || 'synthwave'
+    setTheme(nowTheme)
   }
 
-  const setMode = (value: string) => {
+  const setTheme = (value: string) => {
     // 设置 data-theme
     document.documentElement.setAttribute('data-theme', value)
-    document.documentElement.classList.remove(theme.value)
-    document.documentElement.classList.add('value')
     localStorage.theme = value
     theme.value = value
   }
 
-  const getMode = () => {
-    return theme
-  }
-
-  const ifDark = computed(() => {
-    return darkThemes.includes(theme.value)
-  })
-
   return {
     theme,
-    initMode,
-    setMode,
-    getMode,
-    ifDark,
+    initTheme,
+    setTheme,
   }
 })
