30238e5e49d54c77762e12ee893eb873456520da
Lirous587 <2115883273@qq.com>
2025-11-07 23:45:43 +0800

    feat:Queue组件移除不必要的Teleport依赖

--- packages/Queue/src/layout.vue
+++ packages/Queue/src/layout.vue
@@ -1,39 +1,32 @@
 <template>
-  <CompatiblePortal to="body">
-    <div class="fixed inset-0 pointer-events-none z-[9999] vp-raw">
-      <!-- 左上角 -->
-      <QueuePositionContainer position="top-start" :items="leftTopItems" @remove="handleRemove" />
+  <div v-if="hasItem" class="fixed inset-0 pointer-events-none z-[9999] vp-raw">
+    <!-- 左上角 -->
+    <QueuePositionContainer position="top-start" :items="leftTopItems" @remove="handleRemove" />
 
-      <!-- 顶部中央 -->
-      <QueuePositionContainer
-        position="top-center"
-        :items="topCenterItems"
-        @remove="handleRemove"
-      />
+    <!-- 顶部中央 -->
+    <QueuePositionContainer position="top-center" :items="topCenterItems" @remove="handleRemove" />
 
-      <!-- 右上角 -->
-      <QueuePositionContainer position="top-end" :items="rightTopItems" @remove="handleRemove" />
+    <!-- 右上角 -->
+    <QueuePositionContainer position="top-end" :items="rightTopItems" @remove="handleRemove" />
 
-      <!-- 左下角 -->
-      <QueuePositionContainer
-        position="bottom-start"
-        :items="leftBottomItems"
-        @remove="handleRemove"
-      />
+    <!-- 左下角 -->
+    <QueuePositionContainer
+      position="bottom-start"
+      :items="leftBottomItems"
+      @remove="handleRemove"
+    />
 
-      <!-- 右下角 -->
-      <QueuePositionContainer
-        position="bottom-end"
-        :items="rightBottomItems"
-        @remove="handleRemove"
-      />
-    </div>
-  </CompatiblePortal>
+    <!-- 右下角 -->
+    <QueuePositionContainer
+      position="bottom-end"
+      :items="rightBottomItems"
+      @remove="handleRemove"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import CompatiblePortal from '../../ssr/CompatiblePortal.vue'
 import type { QueueLayoutProps } from './types'
 import QueuePositionContainer from './position-container.vue'
 
@@ -45,6 +38,16 @@ const topCenterItems = computed(() => props.queueMap['top-center'] || [])
 const rightTopItems = computed(() => props.queueMap['top-end'] || [])
 const leftBottomItems = computed(() => props.queueMap['bottom-start'] || [])
 const rightBottomItems = computed(() => props.queueMap['bottom-end'] || [])
+
+const hasItem = computed(() => {
+  return (
+    leftTopItems.value.length ||
+    topCenterItems.value.length ||
+    rightTopItems.value.length ||
+    leftBottomItems.value.length ||
+    rightBottomItems.value.length
+  )
+})
 
 const emit = defineEmits<{
   remove: [id: string]
